94d9112bfb01d7e76c76f3b04c0e5a4330e84b9f
ghulamali <ghulam.ali@cybernest.com>
2023-12-12 13:00:13 +0500

    Models With Roles

---
+++ app/Models/Role.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    // Model factories are used for seeding databases with test data,
+    use HasFactory; //trait
+
+    // By default, Laravel assumes that the table name is the plural form of the model name (e.g., users for the User
+    // if our table name is not exact, then we uses the $table
+    protected $table = 'roles';
+
+    //  Mass assignment involves assigning an array of attributes to a model, typically through methods like create or update.
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+    
+    // relation with users model
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
